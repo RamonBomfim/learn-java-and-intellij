8ad9c5c680d057d7de29d38357e6a1b45fce4c3a
Ramon Paulo Fernandes Bomfim <ramon.fbomfim@hotmail.com>
2022-03-04 23:00:42 -0300

    criando class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -7,11 +7,44 @@ import br.com.dio.model.Gato;
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
-
         System.out.println(gato);
 
-        /*int a = 5;
-        int b = 5;
-        System.out.println("Hello World! " + (a + b));*/
+        Livro livro = new Livro("Técnicas de Invasão", 250);
+        System.out.println(livro);
+
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
